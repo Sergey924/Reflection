e14b88f871fe84dfd65fe5831eaff5b1f27a0b41
Sergey924 <74462379+Sergey924@users.noreply.github.com>
2021-02-16 20:31:38 +0200

    Add files via upload

---
+++ ClassLibrary1/Thermometer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Thermometer
+
+    {
+        public int TemperatCels { get; set; }
+
+        public Thermometer()
+        {
+        }
+    }
+}
